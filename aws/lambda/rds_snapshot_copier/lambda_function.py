423701c2f36c72881be4915871867ec4f155f6fe
Marcus Young <myoung34@my.apsu.edu>
2018-01-01 17:18:52 -0600

    Minor bugfix to RDS/Redshift snapshot copier

--- aws/lambda/rds_snapshot_copier/lambda_function.py
+++ aws/lambda/rds_snapshot_copier/lambda_function.py
@@ -1,12 +1,14 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
 # pylint: disable=superfluous-parens
 import boto3
-from pprint import pprint
 import re
 from operator import itemgetter
 from datetime import datetime, timezone
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError  # noqa: F401
 
 rds_client = boto3.client('rds')
+
 
 def lambda_handler(event, context):
     print('Creating snapshots')
@@ -14,7 +16,6 @@ def lambda_handler(event, context):
         automated_snapshots = [s for s in rds_client.describe_db_snapshots(DBInstanceIdentifier=db_instance['DBInstanceIdentifier'])['DBSnapshots'] if (s['SnapshotType'] == 'automated' and s['Status'] == 'available')]
         manual_snapshots = [s for s in rds_client.describe_db_snapshots(DBInstanceIdentifier=db_instance['DBInstanceIdentifier'])['DBSnapshots'] if (s['SnapshotType'] != 'automated' and s['Status'] == 'available')]
         latest_automated_snapshot = sorted(automated_snapshots, key=itemgetter('SnapshotCreateTime'), reverse=True)[0]
-        latest_manual_snapshot = sorted(manual_snapshots, key=itemgetter('SnapshotCreateTime'), reverse=True)[0]
         identifier = re.search('.+?:(.*)', latest_automated_snapshot['DBSnapshotIdentifier']).group(1)
         print('creating {} from'.format(identifier, latest_automated_snapshot['DBSnapshotIdentifier']))
         try:
@@ -27,7 +28,7 @@ def lambda_handler(event, context):
                         'Value': db_instance['DBInstanceIdentifier'],
                     },
                     {
-                        'Key': 'Managed_By',
+                        'Key': 'Managed_by',
                         'Value': 'lambda:rds_snapshot_copier',
                     },
                 ],
@@ -37,14 +38,14 @@ def lambda_handler(event, context):
             print('Skipping already created snapshot')
             pass
 
-    print('Cleaning old snapshots')
-    manual_snapshots = [s for s in rds_client.describe_db_snapshots(DBInstanceIdentifier=db_instance['DBInstanceIdentifier'])['DBSnapshots'] if (s['SnapshotType'] != 'automated' and s['Status'] == 'available')]
-    for snapshot in manual_snapshots:
-        tags = rds_client.list_tags_for_resource(ResourceName=snapshot['DBSnapshotArn'])['TagList']
-        if (len(tags)>0 and "lambda:rds_snapshot_copier" in [tag['Value'] for tag in tags]):
-            if ((datetime.now(timezone.utc) - snapshot['SnapshotCreateTime']).days > 7):
-                print('removing snapshot with identifier {}'.format(snapshot['DBSnapshotIdentifier']))
-                rds_client.delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
+        print('Cleaning old snapshots')
+        manual_snapshots = [s for s in rds_client.describe_db_snapshots(DBInstanceIdentifier=db_instance['DBInstanceIdentifier'])['DBSnapshots'] if (s['SnapshotType'] != 'automated' and s['Status'] == 'available')]
+        for snapshot in manual_snapshots:
+            tags = rds_client.list_tags_for_resource(ResourceName=snapshot['DBSnapshotArn'])['TagList']
+            if (len(tags) > 0 and "lambda:rds_snapshot_copier" in [tag['Value'] for tag in tags]):
+                if ((datetime.now(timezone.utc) - snapshot['SnapshotCreateTime']).days > 7):
+                    print('removing snapshot with identifier {}'.format(snapshot['DBSnapshotIdentifier']))
+                    rds_client.delete_db_snapshot(DBSnapshotIdentifier=snapshot['DBSnapshotIdentifier'])
 
 
 if __name__ == "__main__":
--- aws/lambda/redshift_snapshot_copier/lambda_function.py
+++ aws/lambda/redshift_snapshot_copier/lambda_function.py
@@ -1,15 +1,18 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
 # pylint: disable=superfluous-parens
 import boto3
 import re
 from operator import itemgetter
 from datetime import datetime, timezone
-from botocore.exceptions import ClientError
+from botocore.exceptions import ClientError  # noqa: F401
 
 redshift_client = boto3.client('redshift')
 
+
 def lambda_handler(event, context):
     for cluster in [instance for instance in redshift_client.describe_clusters()['Clusters']]:
-        print('Creating snapshots')
+        print('Creating snapshots for cluster {}'.format(cluster['ClusterIdentifier']))
         automated_snapshots = [s for s in redshift_client.describe_cluster_snapshots(ClusterIdentifier=cluster['ClusterIdentifier'])['Snapshots'] if (s['SnapshotType'] == 'automated' and s['Status'] == 'available')]
         sorted_automated_snapshots = sorted(automated_snapshots, key=itemgetter('SnapshotCreateTime'), reverse=True)
         if (len(sorted_automated_snapshots) > 0):
@@ -23,39 +26,43 @@ def lambda_handler(event, context):
                 )['Snapshot']
                 redshift_client.create_tags(
                     ResourceName='arn:aws:redshift:{}:{}:snapshot:{}/{}'.format(
-                        create_response['AvailabilityZone'][:-1], 
-                        create_response['OwnerAccount'], 
-                        create_response['ClusterIdentifier'], 
+                        create_response['AvailabilityZone'][:-1],
+                        create_response['OwnerAccount'],
+                        create_response['ClusterIdentifier'],
                         'manual-{}'.format(identifier)
                     ),
                     Tags=[{
-                        'Key': 'Managed_By',
+                        'Key': 'Managed_by',
                         'Value': 'lambda:redshift_snapshot_copier'
                     }]
                 )
+            except redshift_client.exceptions.ClusterSnapshotQuotaExceededFault:
+                pass
             except redshift_client.exceptions.ClientError as e:
-                if ('has already been copied' in str(e)):
+                if ('Cannot create more than' in str(e)):
+                    pass
+                elif ('has already been copied' in str(e)):
                     print('Skipping already created snapshot')
                     pass
                 else:
                     raise(e)
         else:
-            print('No automated snaphots found for cluster {}'.format(cluster))
+            print('No automated snaphots found for cluster {}'.format(cluster['ClusterIdentifier']))
 
-        print('Cleaning old snapshots')
+        print('Clearing old snapshots for cluster {}'.format(cluster['ClusterIdentifier']))
         manual_snapshots = [s for s in redshift_client.describe_cluster_snapshots(ClusterIdentifier=cluster['ClusterIdentifier'])['Snapshots'] if (s['SnapshotType'] != 'automated' and s['Status'] == 'available')]
         for snapshot in manual_snapshots:
             match = re.search('^(manual-).*', snapshot['SnapshotIdentifier'])
             if (match is not None and match.group(1) is not None):
                 response = redshift_client.describe_tags(
                     ResourceName='arn:aws:redshift:{}:{}:snapshot:{}/{}'.format(
-                        snapshot['AvailabilityZone'][:-1], 
-                        snapshot['OwnerAccount'], 
-                        snapshot['ClusterIdentifier'], 
+                        snapshot['AvailabilityZone'][:-1],
+                        snapshot['OwnerAccount'],
+                        snapshot['ClusterIdentifier'],
                         snapshot['SnapshotIdentifier']
                     ),
                     TagKeys=[
-                        'Managed_By',
+                        'Managed_by',
                     ],
                     TagValues=[
                         'lambda:redshift_snapshot_copier',
